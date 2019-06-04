af093e6f353075ddbdfc68041afcbf9b48b319b1
Nickolai Schioldan Winther <nick3846@edu.campusvejle.dk>
2019-06-04 09:51:40 +0200

    index page done

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto&display=swap" rel="stylesheet">
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Games | Forside</title>
+</head>
+<body>
+    <div class="wrapper">
+        <aside>
+            <img class="logo" src="img/logo.png" alt="">
+            <h1 class="header">Games</h1>
+            <nav>
+                <ul>
+                    <li><a class="active" href="index.php">Forside</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="#">Kontakt</a></li>
+                    <li><a href="#">Om os</a></li>
+                    <li><a href="#">Log in</a></li>
+                </ul>
+            </nav>
+        </aside>
+        <main>
+            <img class="banner" src="img/banner.jpg" alt="">
+            <div class="content">
+                <h1 class="header">Games - Et spændende slogan</h1>
+
+                <h2>Mest populære spil</h2>
+                <div class="games-wrapper">
+                    <article class="game">
+                        <h3 class="game-title">Ark survival evolved</h1>
+
+                        <img class="game-image" src="img/ark-survival-evolved.jpg" alt="">
+
+                        <div class="publisher">
+                            <p class="publisher-name">Nickolai Winther</p>
+                            <p class="publicher-time">03-04-2019</p>
+                        </div>
+
+                        <p class="game-description">Frysende, sultende og nøgen – det er dig, strandet på en mystisk ø kaldet ARK. Overvind elementerne, andre spillere og dinosaurer i din kamp for overlevelse.</p>
+                        
+                        <p>Kategorier</p>
+                        <ul class="categories">
+                            <li class="category">Action</li>
+                            <li class="category">Oplevelser</li>
+                            <li class="category">Online Co-op</li>
+                        </ul>
+                        <a href="#" class="price">449</a>
+                    </article>
+
+                    <article class="game">
+                        <h3 class="game-title">Call of Duty WW II</h1>
+
+                        <img class="game-image" src="img/call-of-duty-ww2.jpg" alt="">
+
+                        <div class="publisher">
+                            <p class="publisher-name">Nickolai Winther</p>
+                            <p class="publicher-time">03-04-2019</p>
+                        </div>
+
+                        <p class="game-description">Vend tilbage til det 20. århundredes mest ikoniske krig og den dramatiske kulisse, der oprindeligt var inspiration for den populære Call of Duty-spilserie.</p>
+                        
+                        <p>Kategorier</p>
+                        <ul class="categories">
+                            <li class="category">Action</li>
+                            <li class="category">Første person-skyder</li>
+                            <li class="category">Online Multiplayer</li>
+                        </ul>
+                        <a href="#" class="price">449</a>
+                    </article>
+
+                    <article class="game">
+                        <h3 class="game-title">Diablo III</h1>
+
+                        <img class="game-image" src="img/diablo-3.jpg" alt="">
+
+                        <div class="publisher">
+                            <p class="publisher-name">Nickolai Winther</p>
+                            <p class="publicher-time">03-04-2019</p>
+                        </div>
+
+                        <p class="game-description">Over 13 million players have battled the demonic hordes of Diablo III. Now, it's your turn to join the crusade, and take up arms against the enemies of the mortal realms. </p>
+                        
+                        <p>Kategorier</p>
+                        <ul class="categories">
+                            <li class="category">Action</li>
+                            <li class="category">Rollespil</li>
+                            <li class="category">Online Co-op</li>
+                        </ul>
+                        <a href="#" class="price">449</a>
+                    </article>
+                </div>
+            </div>
+        </main>
+    </div>
+    <footer>
+        <div class="links">
+            <h3>Links</h3>
+            <a href="#">Kundeservice</a>
+            <a href="#">Handelsbetingelser</a>
+            <a href="#">FAQ</a>
+        </div>
+        <div class="social">
+            <h3>Følgs os på:</h3>
+            <div class="social-icons">
+                <a href="#">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-youtube"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+        <div class="newsletter">
+            <h3>Nyhedsbrev</h3>
+            <form>
+                <input type="text" placeholder="Fornavn">
+                <input type="text" placeholder="Efternavn">
+                <input type="email" placeholder="E-mail">
+                <input type="submit" value="Tilmeld">
+            </form>
+        </div>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/5057af1f82.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
